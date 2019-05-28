274e98749559a5931f7803d7df8ed856f2e54e3f
Annie Huang <annie.hn@gmail.com>
2019-05-28 21:00:53 +1000

    create express javascript

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 // const greeting: string = 'Hello';
 //
 // const numbers: number[] = [1,2,3];
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(5000, () => console.log('Server running'));
+
+
